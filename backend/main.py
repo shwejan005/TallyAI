bebd6525542f99db0e1e026d8ee31851f1313fa0
shwejan005 <shwejannn@gmail.com>
2024-12-25 00:33:28 +0530

    Backend done

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from apps.calculator.route import router as calculator_router # type: ignore
 from constants import SERVER_URL, PORT, ENV
+from apps.calculator.route import router as calculator_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -26,7 +26,6 @@ async def root():
     return {"message": "Server is running"}
 
 app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev"))
